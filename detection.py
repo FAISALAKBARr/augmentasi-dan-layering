21e016463b67743530729d36d008e8c898d3013c
FAISALAKBARr <faisalzogg022@gmail.com>
2024-12-07 01:18:56 +0700

    realtime

--- detection.py
+++ detection.py
@@ -158,7 +158,7 @@ def main():
     
     else:
         st.write("Real-time detection mode")
-        camera = cv2.VideoCapture(1)
+        camera = cv2.VideoCapture(2)
         
         if not camera.isOpened():
             st.error("Camera not found or not accessible. Please check your camera setup.")
